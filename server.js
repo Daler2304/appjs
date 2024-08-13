84243575ba103b02a061dbaef71c7df7255bc58f
Daler2304 <110163963+Daler2304@users.noreply.github.com>
2024-08-13 21:25:53 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`on port ${process.env.PORT || 3000}`);
+});
+
+app.get('/', async (req, res) => {
+    res.send('<h1>Hello Daler</h1>');
+});
