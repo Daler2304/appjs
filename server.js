614c9398035da21edbac87e38515bdfee69d6283
Daler2304 <110163963+Daler2304@users.noreply.github.com>
2024-09-08 22:23:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware для определения поддомена
+app.use((req, res, next) => {
+    const host = req.hostname.split('.');
+    const subdomain = host.length > 2 ? host[0] : null;
 
+    if (subdomain === 'timetable') {
+        // Если поддомен "timetable", возвращаем специальную страницу
+        res.sendFile(path.join(__dirname, 'public/templates/timetable.html'));
+    } else {
+        next();
+    }
+});
+
+// Основная страница
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/templates/index.html'));
 });
 
+// Страница администратора
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/templates/admin.html'));
 });
 
+// Запуск сервера
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
