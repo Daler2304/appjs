2bccde161e43dffe67f691c5fdeb3ebae8a88aec
Daler2304 <110163963+Daler2304@users.noreply.github.com>
2024-10-23 14:24:22 +0300

    Add files via upload

--- templates/script.js
+++ templates/script.js
@@ -11,7 +11,7 @@ function setUnderline(lang) {
     const enElement = document.getElementById("lang-en");
     const ruElement = document.getElementById("lang-ru");
 
-    fetch("lang.json") // Укажите правильный путь к вашему файлу
+    fetch("/lang.json")
         .then((response) => {
             if (!response.ok) {
                 throw new Error("Network response was not ok");
@@ -19,18 +19,33 @@ function setUnderline(lang) {
             return response.json();
         })
         .then((data) => {
-            // Обновление текста на основе выбранного языка
-            const raspElement = document.querySelector(".rasp");
-            raspElement.textContent = data[lang].timetable;
+            // Обновляем тексты на странице на выбранный язык
+            document.querySelector(".rasp").textContent = data[lang].timetable;
+            document.querySelector(".main-app p").textContent =
+                data[lang].timetable;
 
-            const currentYearElement = document.querySelector(".main-app p");
-            currentYearElement.textContent = data[lang].current_year;
+            // Обновляем все элементы footer
+            const footerItems = document.querySelectorAll(".footer-list li");
+            footerItems[0].textContent = data[lang].admin;
+            footerItems[1].textContent = data[lang].contacts;
+            footerItems[2].textContent = data[lang].offsite;
+            footerItems[3].textContent = data[lang].more;
+            // Обновляем текст футера
+            document.querySelector(".footer-3").textContent =
+                data[lang].footer_text;
+
+            // Обновляем группы
+            const groupElements = document.querySelectorAll(".groups");
+            const groups = data[lang].groups;
+            groupElements.forEach((element, index) => {
+                element.textContent = groups[index];
+            });
         })
         .catch((error) => {
             console.error("Error fetching JSON:", error);
         });
 
-    // Установка выделения для языка
+    // Устанавливаем подчеркивание для выбранного языка
     if (lang === "en") {
         ruElement.style.textDecoration = "none";
         enElement.style.textDecoration = "underline";
